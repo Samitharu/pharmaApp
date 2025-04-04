3968a8bf63bc1843724a674fb53b3eae31e98119
Samitha Lowe <58819808+Samitharu@users.noreply.github.com>
2025-04-04 16:03:07 +0530

    update Purchase order

--- public/asset/js/procument/purchaseOrder.js
+++ public/asset/js/procument/purchaseOrder.js
@@ -10,7 +10,7 @@ let debounceTimer; // Store the timer
 function fetchItemDetails(event) {
     let inputField = event.target;
     let barcode = inputField.value;
-
+    clickedRowObject = inputField.closest("tr");
     // Clear the previous timer to reset the debounce delay
     clearTimeout(debounceTimer);
 
@@ -21,16 +21,23 @@ function fetchItemDetails(event) {
                 .then(response => response.json())
                 .then(data => {
                     if (data.success) {
-                        let row = inputField.closest("tr");
-                        console.log(row);
+                        let isDuplicate = Array.from(document.querySelectorAll(".item-code"))
+                            .some(input => input !== clickedRowObject.querySelector(".item-code") && input.value.trim() === data.item.item_code);
+                        console.log(isDuplicate);
 
-                        row.querySelector(".item-name").value = data.item.name;
-                        row.querySelector(".item-code").value = data.item.item_code;
-                        row.querySelector(".pack-size").value = data.item.pack_size;
-                        row.querySelector(".purchase-price").value = data.item.purchase_price;
-                        row.querySelector(".wholesale-price").value = data.item.wholesale_price;
-                        row.querySelector(".retail-price").value = data.item.retail_price;
-                        row.querySelector(".qty").focus();
+                        if (!isDuplicate) {
+                            let row = inputField.closest("tr");
+
+                            row.querySelector(".item-name").value = data.item.name;
+                            row.querySelector(".item-code").value = data.item.item_code;
+                            row.querySelector(".pack-size").value = data.item.pack_size;
+                            row.querySelector(".purchase-price").value = data.item.purchase_price;
+                            row.querySelector(".wholesale-price").value = data.item.wholesale_price;
+                            row.querySelector(".retail-price").value = data.item.retail_price;
+                            row.querySelector(".qty").focus();
+                        }else{
+                            alertify.error('Product can not be duplicated');
+                        }
                     }
                 })
                 .catch(error => console.error("Error fetching item:", error));
@@ -84,8 +91,8 @@ function loadItemDetails(event) {
             if (data.success) {
                 let isDuplicate = Array.from(document.querySelectorAll(".item-code"))
                     .some(input => input !== clickedRowObject.querySelector(".item-code") && input.value.trim() === data.item.product_code);
-                
-                if(!isDuplicate){
+
+                if (!isDuplicate) {
                     let row = clickedRowObject;
                     row.querySelector(".item-name").value = data.item.product_name;
                     row.querySelector(".item-code").value = data.item.product_code;
@@ -94,10 +101,10 @@ function loadItemDetails(event) {
                     row.querySelector(".wholesale-price").value = data.item.wholesale_price;
                     row.querySelector(".retail-price").value = data.item.retail_price;
                     row.querySelector(".qty").focus();
-                }else{
+                } else {
                     alertify.error('Product can not be duplicated');
                 }
-               
+
             }
         })
         .catch(error => console.error("Error fetching item:", error));
@@ -105,6 +112,20 @@ function loadItemDetails(event) {
 }
 
 //Calculate value / Gross /Net Totals
-function calculateValue(){
+function calculateValue() {
+    let transactionTable = document.getElementById('transactionsTable');
+    const rows = transactionTable.querySelectorAll("tbody tr");
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => {
+        rows.forEach(row => {
 
+            let itemCodeInput = row.querySelector(".item-code");
+            if (itemCodeInput.value.length > 0) {
+                let itemQty = row.querySelector(".qty").value;
+                console.log(itemQty);
+
+            }
+
+        })
+    }, 1000);
 }
